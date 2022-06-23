b6e9d75fc91fa7508ae90656c5b8d7d111a7a646
unknown <abhisheksrivastava9719@gmail.com>
2022-06-23 19:48:27 +0530

    Condition for the wage of the employee

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using EmployeeWage;
+Console.WriteLine("Welcome to Employee Wage Computation program");
 Console.WriteLine("Enter 1 - Part time & 2- for full time employee");    //either part time or full time employee
 int choice = Convert.ToInt16(Console.ReadLine());
 if (choice < 1 || choice > 2)
 {
     Console.WriteLine("Wrong input");
-    //Environment.Exit(1);                       //for exiting the code
 }
 else
 {
